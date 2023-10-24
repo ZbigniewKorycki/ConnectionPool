f95a8787648d5cc001723d912e1ce1a159b058ca
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-24 14:28:00 +0200

    move init of connection pool to class, create read_data/write_data, and close connection pool

--- connection_pool.py
+++ connection_pool.py
@@ -1,57 +1,74 @@
 import psycopg2.pool
 import psycopg2.extensions
-import threading
 from db_config import config
-from fake_data_generator import generate_record
-import time
 
 config_data = config()
 
 
-def get_connection_pool():
-    return psycopg2.pool.ThreadedConnectionPool(
-        minconn=5,
-        maxconn=10,
-        database=config_data['database'],
-        user=config_data['user'],
-        password=config_data['password'],
-        host=config_data['host'],
-        port=config_data['port'])
+class ConnectionPool:
+    def __init__(self, database=config_data['database'], user=config_data['user'], password=config_data['password'],
+                 host=config_data['host'], port=config_data['port'], min_connections=5, max_connections=10):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.min_connections = min_connections
+        self.max_connections = max_connections
+        self.connection_pool = None
+        self.create_connection_pool()
 
+    def create_connection_pool(self):
+        new_connection_pool = psycopg2.pool.ThreadedConnectionPool(
+            minconn=self.min_connections,
+            maxconn=self.max_connections,
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port)
+        self.connection_pool = new_connection_pool
 
-def insert_records(n, n_threads, query, params, connection_pool):
-    connection = connection_pool.getconn()
-    cursor = connection.cursor()
-    try:
-        for _ in range(n//n_threads):
+    def read_data(self, query, params=None, fetch=all):
+        connection = self.connection_pool.getconn()
+        cursor = connection.cursor()
+        connection.autocommit = False
+        try:
             cursor.execute(query, params)
-        connection.commit()
-    except Exception as e:
-        print(e)
-        connection.rollback()
-    finally:
-        cursor.close()
-        connection_pool.putconn(connection)
+            if fetch == "one":
+                result = cursor.fetchone()
+            else:
+                result = cursor.fetchall()
+            data = [dict(zip([key[0] for key in cursor.description], row)) for row in result]
+        except (Exception, psycopg2.DatabaseError) as error:
+            connection.rollback()
+            return error
+        else:
+            connection.commit()
+            return data
+        finally:
+            cursor.close()
+            self.connection_pool.putconn(connection)
 
+    def write_data(self, query, params=None):
+        connection = self.connection_pool.getconn()
+        cursor = connection.cursor()
+        connection.autocommit = False
+        try:
+            cursor.execute(query, params)
+        except (Exception, psycopg2.DatabaseError) as error:
+            connection.rollback()
+            return False
+        else:
+            connection.commit()
+            return True
+        finally:
+            cursor.close()
+            self.connection_pool.putconn(connection)
 
-def create_records(n=100000, n_threads=10):
-    query = "INSERT INTO customers (first_name, last_name, email, phone_number, address) VALUES (%s, %s, %s, %s, %s)"
-    params = generate_record()
-    start = time.perf_counter()
-    connection_pool = get_connection_pool()
-    threads = []
-    for _ in range(n_threads):
-        thread = threading.Thread(target=insert_records, args=(n, n_threads, query, params, connection_pool))
-        threads.append(thread)
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-    print(threads)
-    stop = time.perf_counter()
-    print(f"Time of execution: {stop-start}")
+    def close_connection_pool(self):
+        self.connection_pool.closeall()
 
 
 if __name__ == '__main__':
-    create_records()
-
+    connection_pool = ConnectionPool()
